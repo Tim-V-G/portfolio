c004686cb3bac0d198fde9d20a5e671dba32d1b0
Tim <tim.van.gestel@sintlucasedu.nl>
2023-10-30 15:43:03 +0100

    upload from github classroom to public github repository

---
+++ projects.js
@@ -0,0 +1,21 @@
+function translatePage(language) {
+    fetch('../lang.json')
+        .then(response => response.json())
+        .then(translations => {
+            const elements = document.querySelectorAll('.translatable');
+            elements.forEach(element => {
+                const key = element.getAttribute('data-key');
+                const translation = translations[language][key];
+                element.textContent = translation;
+            });
+        })
+        .catch(error => console.error('Error loading translations', error));
+}
+
+translatePage('en');
+
+const languageSelectorProjects = document.querySelector('.language-selectorProjects');
+languageSelectorProjects.addEventListener('change', function () {
+    const selectedLanguageProject = languageSelectorProjects.value;
+    translatePage(selectedLanguageProject);
+});
---
+++ script.js
@@ -0,0 +1,57 @@
+const carousel = document.querySelector("#carousel");
+const container = document.querySelector(".carousel-container");
+const images = document.querySelectorAll(".carouselImage");
+const prevButton = document.querySelector(".btn-previous");
+const nextButton = document.querySelector(".btn-next");
+let currentIndex = 0;
+
+function showImage(index) {
+    const translateValue = -100 * index; // Calculate the translation
+    container.style.transform = `translateX(${translateValue}%)`;
+}
+
+prevButton.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+});
+
+nextButton.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+});
+
+// Initial display
+showImage(currentIndex);
+
+function translatePage(language) {
+    fetch('lang.json')
+        .then(response => response.json())
+        .then(translations => {
+            const elements = document.querySelectorAll('.translatable');
+            elements.forEach(element => {
+                const key = element.getAttribute('data-key');
+                const translation = translations[language][key];
+                element.textContent = translation;
+            });
+        })
+        .catch(error => console.error('Error loading translations', error));
+}
+
+translatePage('en');
+
+const languageSelector = document.getElementById('language-selector');
+languageSelector.addEventListener('change', function () {
+    const selectedLanguage = languageSelector.value;
+    translatePage(selectedLanguage);
+});
+
+let state = 0;
+function openNav(){
+    if (state == 0) {
+        document.getElementById('navmenu').style.display = "initial";
+        state = 1;
+    } else {
+        document.getElementById('navmenu').style.display = "none";
+        state = 0;
+    }
+}
